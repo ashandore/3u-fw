fc3e4ed77005031e1e04338ddd9df538abc0e544
George Harris <george@luminairecoffee.com>
2019-10-26 11:50:27 -0700

    try_t refactor. use result visitation to do stuff with the conversion.

--- include/application.hh
+++ include/application.hh
@@ -12,14 +12,15 @@ namespace hw = stm32g4::driver;
 
 class application {
     using uart_t = hw::uart<false>;
-
+    using log_out_t = utl::logger::output<uart_t>;
+    
     utl::construct<uart_t>                      m_uart;
-    utl::logger::output<uart_t>                 m_application_output;
+    utl::construct<log_out_t>                   m_application_output;
     utl::logger::push_output                    m_logger_setup;
     hw::ucpd                                    m_ucpd;
     hw::pin::push_pull                          m_power_switch;
     utl::construct<hw::adc>                     m_adc;
-    hw::adc::adc_channel_t                      m_current_sense;
+    utl::construct<hw::adc::adc_channel_t>      m_current_sense;
     utl::construct<hw::pwm::source>             m_led_pwm_source;
     utl::construct<hw::pwm::channel_t>          m_led_pwm;
 public:
--- src/application.cc
+++ src/application.cc
@@ -10,18 +10,35 @@ extern "C" void SysTick_Handler(void) {
     //this may need a call to   HAL_SYSTICK_IRQHandler();
 }
 
+extern "C" void TIM3_IRQHandler(void) {
+
+    // __HAL_TIM_CLEAR_IT(&m_handle, TIM_IT_UPDATE);
+}
+
 application::application() :
     m_uart{USART2, 3000000u},
-    m_application_output{m_uart},
-    m_logger_setup{&m_application_output},
+    m_application_output{utl::try_t{m_uart}},
+    m_logger_setup{m_application_output.has_value() ? &m_application_output.value() : nullptr},
     m_ucpd{},
     m_power_switch{GPIOA, GPIO_PIN_1, utl::driver::pin::active_level::high, true, GPIO_NOPULL, GPIO_SPEED_FREQ_MEDIUM},
     m_adc{ADC1, 1000u, 3.3f},
-    m_current_sense{m_adc ? &m_adc.value() : nullptr, ADC_CHANNEL_1},
+    m_current_sense{utl::try_t{m_adc}, ADC_CHANNEL_1},
     m_led_pwm_source{TIM3, 5000u},
-    m_led_pwm{utl::dependent_t{}, utl::try_t{m_led_pwm_source}, hw::pwm::channel_id::CHANNEL_1}
+    m_led_pwm{utl::try_t{m_led_pwm_source}, hw::pwm::channel_id::CHANNEL_1}
 {
+
+    //dimensions along which construct/try could have policies:
+    // - whether to try unboxing by default or not (need to have a forward equivalent to try...)
+    // - what to do when unboxing fails - where should the error come from?
+    //the ability to specify a fallback value would be useful.
+    //  it'sagraph
+    //the ability to specify an error code in case of unboxing failure would be useful.
+    //what if it was actually a dependency graph?
+    //make a try function.
+
     if(!m_uart) while(1);
+
+    
 }
 
 void application::start(void)
@@ -36,17 +53,15 @@ void application::start(void)
 void application::loop(void)
 {
     HAL_Delay(1000);
-    if(m_adc) {
+
+    m_current_sense.visit([&](auto& sense) {
         auto calculate_current = [](const uint16_t conversion) {
             return 161u*static_cast<uint32_t>(conversion)/100u;
         };
 
-        auto conv = m_current_sense.conversion();
-        if(conv) {
-            auto current = calculate_current(conv.value());
+        sense.conversion().visit([&](uint16_t& conv) {
+            auto current = calculate_current(conv);
             utl::log("Load current: %dmA", current);
-        } else {
-            utl::log("conversion failed.");
-        }
-    }
+        });
+    });
 }
