098a53d5c4753da608b77aa7207785ba652d0e7d
George Harris <george@luminairecoffee.com>
2019-10-27 12:25:52 -0700

    Working leds. Not crazy about the driver.

--- include/application.hh
+++ include/application.hh
@@ -7,13 +7,16 @@
 #include <driver/adc.hh>
 #include <driver/ucpd.hh>
 #include <driver/pwm.hh>
+#include "ws2812.hh"
 
 namespace hw = stm32g4::driver;
 
 class application {
+public:
     using uart_t = hw::uart<false>;
     using log_out_t = utl::logger::output<uart_t>;
-    
+    using leds_t = ws2812::spi<85, hw::pwm::source>;
+private:
     utl::construct<uart_t>                      m_uart;
     utl::construct<log_out_t>                   m_application_output;
     utl::logger::push_output                    m_logger_setup;
@@ -23,6 +26,8 @@ class application {
     utl::construct<hw::adc::adc_channel_t>      m_current_sense;
     utl::construct<hw::pwm::source>             m_led_pwm_source;
     utl::construct<hw::pwm::channel_t>          m_led_pwm;
+    utl::construct<leds_t>                      m_leds;
+    uint32_t                                    m_march_count = 0;
 public:
     application();
     void start(void);
--- src/application.cc
+++ src/application.cc
@@ -10,9 +10,26 @@ extern "C" void SysTick_Handler(void) {
     //this may need a call to   HAL_SYSTICK_IRQHandler();
 }
 
-extern "C" void TIM3_IRQHandler(void) {
+static application::leds_t* s_leds = nullptr;
 
-    // __HAL_TIM_CLEAR_IT(&m_handle, TIM_IT_UPDATE);
+extern "C" void TIM3_IRQHandler(void) {
+    s_leds->update_pwm();   
+}
+
+
+void DMA1_Stream3_IRQHandler(void)
+{
+    Application::getInstance().dmaRxISR();
+}
+
+void DMA1_Stream4_IRQHandler(void)
+{
+    Application::getInstance().dmaTxISR();
+}
+
+void SPI2_IRQHandler(void)
+{
+    Application::getInstance().spiISR();
 }
 
 application::application() :
@@ -23,8 +40,9 @@ application::application() :
     m_power_switch{GPIOA, GPIO_PIN_1, utl::driver::pin::active_level::high, true, GPIO_NOPULL, GPIO_SPEED_FREQ_MEDIUM},
     m_adc{ADC1, 1000u, 3.3f},
     m_current_sense{utl::try_t{m_adc}, ADC_CHANNEL_1},
-    m_led_pwm_source{TIM3, 5000u},
-    m_led_pwm{utl::try_t{m_led_pwm_source}, hw::pwm::channel_id::CHANNEL_1}
+    m_led_pwm_source{TIM3, 1000u},
+    m_led_pwm{utl::try_t{m_led_pwm_source}, hw::pwm::channel_id::CHANNEL_1, hw::pwm::polarity::ACTIVE_HIGH},
+    m_leds{SPI1}
 {
 
     //dimensions along which construct/try could have policies:
@@ -37,8 +55,7 @@ application::application() :
     //make a try function.
 
     if(!m_uart) while(1);
-
-    
+    if(m_leds) s_leds = &m_leds.value();
 }
 
 void application::start(void)
@@ -48,11 +65,35 @@ void application::start(void)
 
     m_power_switch.set_state(true);
     if(!m_adc) utl::log("ADC initialization failed: %s", m_adc.error().message().data());
+
+    if(!m_leds) utl::log("leds failed to initialize.");
+    
+
+    // m_led_pwm_source.visit([] (auto& source) {
+    //     source.set_period_ns(1250);
+    //     if(source.start()) {
+    //         utl::log("started pwm source");
+    //     } else {
+    //         utl::log("couldn't start pwm source");
+    //     }
+    // });
+
+    // m_led_pwm.visit([] (auto& pwm) {
+    //     pwm.set_width_ns(500);
+    //     if(pwm.start()) {
+    //         utl::log("started pwm");
+    //         utl::log("pulse width is %duS", pwm.width_ns());
+    //     } else {
+    //         utl::log("couldn't start pwm");
+    //     }
+    // });
+    NVIC_SetPriority(TIM3_IRQn, 0);
+    NVIC_SetPriority(SysTick_IRQn, 1);
 }
 
 void application::loop(void)
 {
-    HAL_Delay(1000);
+    HAL_Delay(50);
 
     m_current_sense.visit([&](auto& sense) {
         auto calculate_current = [](const uint16_t conversion) {
@@ -61,7 +102,21 @@ void application::loop(void)
 
         sense.conversion().visit([&](uint16_t& conv) {
             auto current = calculate_current(conv);
+            utl::maybe_unused(current);
             utl::log("Load current: %dmA", current);
         });
+    });
+
+    m_leds.visit([&] (auto& leds) {
+        for(uint32_t idx=0; idx < leds.count(); idx++) {
+            if((idx + m_march_count) % 3 == 0) leds[idx] = red;
+            if((idx + m_march_count) % 3 == 1) leds[idx] = green;
+            if((idx + m_march_count) % 3 == 2) leds[idx] = blue;
+        }
+        if(leds.write()) {
+            m_march_count++;
+        } else {
+            utl::log("couldn't write led data");
+        }
     });
 }
